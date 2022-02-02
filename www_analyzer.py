f7668166852f1e432e6e653b491fc5bc34ca711b
root <root@localhost.localdomain>
2022-02-02 12:33:18 +0100

    v.0.2

--- www_analyzer.py
+++ www_analyzer.py
@@ -4,38 +4,50 @@ print(' ---------- WWW-ANALYZER ----------')
 option = -1
 
 while option != '0':
-        print('Wybierz opcję:')
-        print('0. Zakończ program')
-        print('1. Wyświetl status strony WWW')
-        print('2. Wypisz zawartość strony WWW')
+        print('Wybierz opcje:')
+        print('0. Zakoncz program')
+        print('1. Wyswietl status strony WWW')
+        print('2. Wypisz zawartosc strony WWW')
+        print('3. Wyswietl naglowki odpowiedzi')
 
-        option = input('Wybieram opcję: ')
+        option = input('Wybieram opcje: ')
 
         if option == '1':
-                link = input('Podaj pełny link do strony WWW (z http:// lub https://): ')
+                link = input('Podaj pelny link do strony WWW (z http:// lub https://): ')
                 if link[0:5] != 'https' and link[0:4] != 'http':
                         print('Brak informacji o protokole (http:// lub https://).')
                 else:
-                        print('Wykryto informację o protokole (http/https).')
+                        print('Wykryto informacje o protokole (http/https).')
                         www_status = requests.get(link)
                         print('*' * 50)
                         print('Status strony {} to {}'.format(link, www_status))
                         print('*' * 50)
         elif option == '2':
-                link = input('Podaj pełny link do strony WWW: (z http:// lub https://): ')
+                link = input('Podaj pelny link do strony WWW: (z http:// lub https://): ')
                 if link[0:5] != 'https' and link[0:4] != 'http':
                         print('Brak informacji o protokole (http:// lub https://).')
                 else:
-                        print('Wykryto informację o protokole (http/https).')
+                        print('Wykryto informacje o protokole (http/https).')
                         www_status = requests.get(link)
                         www_text = www_status.text
                         print('*' * 50)
-                        print('Zawartość strony WWW {}: '.format(link))
+                        print('Zawartosc strony WWW {}: '.format(link))
                         print(www_text)
-                        print('*' * 50)
+                        print('*' * 50)  
+        elif option == '3':
+                link = input('Podaj pelny link do strony WWW: (z http:// lub https://): ')
+                if link[0:5] != 'https' and link[0:4] != 'http':
+                        print('Brak informacji o protokole (http:// lub https://).')
+                else:
+                        print('Wykryto informacje o protokole (http/https).')
+                        www_status = requests.get(link)   
+                        www_headers = www_status.headers
+                        print('Naglowki strony WWW {}:'.format(link))
+                        print(www_headers)   
         elif option == '0':
-                print('---------- BYE ----------')
+                print('')
         else:
-                print('Podano błędą opcję!')
+                print('Podano bledna opcje!')
 
-print('********** KONIEC PROGRAMU **********'
+print('********** KONIEC PROGRAMU **********')
+print()
